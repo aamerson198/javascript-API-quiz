8a4e5c284e16908d2a52c403412841e5212fa909
Shelby Cignetti <shelby.cignetti@gmail.com>
2020-09-25 13:06:28 -0400

    added css for start and started timer

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,6 +5,21 @@ var question = document.getElementById("question");
 var choiceA = document.getElementById("A");
 var choiceB = document.getElementById("B");
 var choiceC = document.getElementById("C");
-var counter = document.getElementById("counter");
 var timeGauge = document.getElementById("timeGauge");
 var scoreDiv = document.getElementById("scoreContainer");
+
+
+start.addEventListener("click",startQuiz);
+
+// start quiz
+function startQuiz(){
+    timer = setInterval(counter(),1000); // 1000ms = 1s
+    start.style.display = "none";
+    askQuestions();
+    quiz.style.display = "block";
+    
+}
+
+function askQuestions(){
+    
+}
